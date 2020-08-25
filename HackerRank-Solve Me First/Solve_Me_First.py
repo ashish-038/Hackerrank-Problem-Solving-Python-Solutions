b93cff54d29107a971562c5d0ab55453e4b143b0
ashish-038 <ashishladi833@gmail.com>
2020-08-25 20:35:10 +0530

    Added new file fork

--- HackerRank-Solve Me First/Solve_Me_First.py
+++
@@ -1,7 +0,0 @@
-def solveMeFirst(a,b):
-    return a+b
-
-num1 = int(input())
-num2 = int(input())
-value = solveMeFirst(num1,num2)
-print(value)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Hackerrank-Problem-Solving-Python-Solutions
-This repo consists the solution of hackerrank problem solving solutions in python
